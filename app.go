d1201b31a22aa171ec4cc8207fc00ddbd3f83cf8
rob passmore <rpassmore@gmail.com>
2018-06-14 16:12:57 +0100

    Add support for cheerLights
    getting colour value from twitter

--- app.go
+++ app.go
@@ -5,43 +5,77 @@ import (
   "time"
   "github.com/lucasb-eyer/go-colorful"
   "github.com/ikester/blinkt"
+  "io/ioutil"
+  "log"
+  "encoding/json"
+  "net/http"
 )
 
-const (
-  NUM_PIXELS = 8
-)
+const numPixels = 8
+const cheerLightURL = "http://api.thingspeak.com/channels/1417/field/2/last.json"
 
-
-var  spacing = 360 / 16
-var  hue = 0
-
+type cheerlight struct {
+  Colour string `json:"field2"`
+}
 
 func main() {
-  fmt.Println("Hello, playground")
-    
-  blinkt := blinkt.NewBlinkt()
-  blinkt.ShowAnimOnStart = true
-  blinkt.CaptureExit = true
-  blinkt.ShowAnimOnExit = true
-  blinkt.ClearOnExit = true
+  fmt.Println("Cheerlights started")
 
-  blinkt.Setup()
+  theBlinkt := blinkt.NewBlinkt()
+  theBlinkt.ShowAnimOnStart = true
+  theBlinkt.CaptureExit = true
+  theBlinkt.ShowAnimOnExit = true
+  theBlinkt.ClearOnExit = true
+
+  theBlinkt.Setup()
+  time.Sleep(30 * time.Second)
 
   //loop forever
   for {
-    hue = int(time.Now().Unix() * 100) % 360
-    for i := 0; i < NUM_PIXELS; i++ {
-      var offset = i * spacing
-      var h = ((hue + offset) % 360) / 360.0
-      var c = colorful.Hsv(float64(h), 1.0, 1.0)
 
-      blinkt.SetPixel(i, int(c.R), int(c.G), int(c.B))
-      blinkt.Show()
-
-      fmt.Printf("Seting pixel %d to %v, %v, %v", i, c.R, c.G, c.B)
-      time.Sleep(100 * time.Millisecond)
+    var c, getErr = colorful.Hex(getCheerlightColours())
+    if getErr != nil {
+      log.Println(getErr)
     }
+    var r, g, b = c.RGB255()
+
+    //theBlinkt.SetPixel(i, int(c.R), int(c.G), int(c.B))
+    theBlinkt.SetAll(int(r), int(g), int(b))
+    theBlinkt.Show()
+
+    fmt.Printf("Seting pixel %d to %v, %v, %v", i, c.R, c.G, c.B)
+    time.Sleep(100 * time.Second)
   }
+}
+
+func getCheerlightColours() (string) {
+  var netClient = &http.Client{
+    Timeout: time.Second * 3,
+  }
+  resp, getErr := netClient.Get(cheerLightURL)
+
+  if getErr != nil {
+    log.Panic(getErr)
+  }
+
+  if resp.Body != nil {
+    defer resp.Body.Close()
+  }
+
+  body, readErr := ioutil.ReadAll(resp.Body)
+
+  if readErr != nil {
+    log.Panic(getErr)
+  }
+
+  result := cheerlight{}
+
+  parseErr := json.Unmarshal(body, &result)
+  if parseErr != nil {
+    log.Panic("Can't parse response")
+    return "#0000"
+  }
+  return result.Colour
 }
 
 //func check(err error) {
